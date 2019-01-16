4b1738dc1459625fba6021a2c419c73b7095d6da
Timo <ttimoww@gmail.com>
2019-01-16 14:42:21 +0100

    HTML + CSS done

---
+++ assets/js/eventlisteners.js
@@ -0,0 +1,15 @@
+/**
+* TRIGGER: 'close' on popup
+* 1. Hides the popup window for more info
+*/
+$('#close-more-info').click(function(){
+  $('.more-info').fadeOut();
+})
+
+/**
+* TRIGGER: 'close' on popup
+* 1. Hides the popup window for add restaurant
+*/
+$('#close-add-restaurant').click(function(){
+  $('.add-restaurant').fadeOut();
+})
