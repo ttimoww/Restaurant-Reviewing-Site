e6486c831b8ad334128cf8182b3e826dce36400e
Timo <ttimoww@gmail.com>
2019-02-04 19:32:01 +0100

    Removed some comments. Small changes on AJAX requests.

--- assets/js/initPage.js
+++ assets/js/initPage.js
@@ -24,7 +24,7 @@ function initMap() {
       };
 
       infoWindow.setPosition(pos);
-      infoWindow.setContent('Your location.');
+      infoWindow.setContent('Your location');
       infoWindow.open(map);
       map.setCenter(pos);
     }, function() {
